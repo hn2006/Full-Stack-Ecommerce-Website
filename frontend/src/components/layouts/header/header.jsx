f9b81b115385bab87e2cd9d368d32b315f9393d6
Harshul <nawaniharshul2@gmail.com>
2025-05-10 22:18:44 +0530

    corrected header loading issue

--- frontend/src/components/layouts/header/header.jsx
+++ frontend/src/components/layouts/header/header.jsx
@@ -1,4 +1,4 @@
-import './header.css'
+import './header.css';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { Link, useNavigate } from 'react-router-dom';
@@ -8,96 +8,107 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import { SpeedDial, SpeedDialAction } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../../actions/userActions';
+
 const actions = [
     { icon: <AccountBoxIcon />, name: 'account' },
     { icon: <HomeIcon />, name: 'home' },
     { icon: <DashboardIcon />, name: 'dashboard' },
-    { icon:<LocalMallIcon></LocalMallIcon>,name:'orders'},
+    { icon: <LocalMallIcon />, name: 'orders' },
     { icon: <LogoutIcon />, name: 'logout' },
 ];
-
 
 const Header = () => {
     const { isauthenticated, user } = useSelector(state => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-
-    const logoutHandler = async () => {
+    const logoutHandler = () => {
         navigate('/home');
         dispatch(logoutUser());
-    }
+    };
 
-
-
+    const avatarSrc = user?.avatar?.url || '/images/second.jpg';
 
     return (
-
         <header className="header">
-            <li className='header-logo'><span height='20px' width='20px' style={{position:'relative'}}><img style={{height:'30px',width:'30px',marginRight:'10px',borderRadius:'7px'}} src='/images/new_logo.jpg' alt='logo'></img></span><span>Blue</span> Mart</li>
-            <nav className="navbar">
+            <li className='header-logo'>
+                <span style={{ position: 'relative' }}>
+                    <img
+                        style={{ height: '30px', width: '30px', marginRight: '10px', borderRadius: '7px' }}
+                        src='/images/new_logo.jpg'
+                        alt='logo'
+                    />
+                </span>
+                <span>Blue</span> Mart
+            </li>
 
-                <Link to={'/home'}><li className='header-items'>Home</li></Link>
-                <Link to={'/aboutus'}><li className='header-items'>About us</li></Link>
-                <Link to={'/products'}><li className='header-items'>Products</li></Link>
-                <Link to={'/contactUs'}><li className='header-items'>contact us</li></Link>
+            <nav className="navbar">
+                <Link to="/home"><li className="header-items">Home</li></Link>
+                <Link to="/aboutus"><li className="header-items">About us</li></Link>
+                <Link to="/products"><li className="header-items">Products</li></Link>
+                <Link to="/contactUs"><li className="header-items">Contact us</li></Link>
             </nav>
 
-            <div className='nav-icons'><Link to={'/user/cart'}><LocalMallIcon fontSize='large' sx={{ cursor: 'pointer', marginRight: '2.2rem', color: 'white', fontSize: '3.2rem' }}></LocalMallIcon></Link> {
-                (!isauthenticated) ? (<Link to={'/account'}><AccountBoxIcon fontSize='large' sx={{ cursor: 'pointer', fontSize: '3.2rem', color: 'white' }}></AccountBoxIcon></Link>) :
-                    (<div>
-                        <SpeedDial
-                            ariaLabel="SpeedDial basic example"
-                            sx={{ position: 'fixed', top: 16, right: 80, color: 'purple' }}
-                            icon={<img height={50} width={50} style={{ border: '2px solid red', borderRadius: '50%' }} src={(user.avatar) ? user.avatar.url : '/images/second.jpg'} alt='na'></img>}
-                            direction='down'
-                        >
-                            {actions.map((action) => {
+            <div className='nav-icons'>
+                <Link to="/user/cart">
+                    <LocalMallIcon
+                        fontSize="large"
+                        sx={{ cursor: 'pointer', marginRight: '2.2rem', color: 'white', fontSize: '3.2rem' }}
+                    />
+                </Link>
 
-                                if (action.name === 'logout') {
-
-                                    return <SpeedDialAction
+                {!isauthenticated ? (
+                    <Link to="/account">
+                        <AccountBoxIcon
+                            fontSize="large"
+                            sx={{ cursor: 'pointer', fontSize: '3.2rem', color: 'white' }}
+                        />
+                    </Link>
+                ) : (
+                    <SpeedDial
+                        ariaLabel="User Menu"
+                        sx={{ position: 'fixed', top: 16, right: 80, color: 'purple' }}
+                        icon={
+                            <img
+                                height={50}
+                                width={50}
+                                style={{ border: '2px solid red', borderRadius: '50%' }}
+                                src={avatarSrc}
+                                alt="User avatar"
+                            />
+                        }
+                        direction="down"
+                    >
+                        {actions.map((action) => {
+                            if (action.name === 'logout') {
+                                return (
+                                    <SpeedDialAction
                                         key={action.name}
                                         icon={action.icon}
                                         tooltipTitle={action.name}
                                         onClick={logoutHandler}
-                                        className='speeddial'
-
+                                        className="speeddial"
                                     />
+                                );
+                            } else {
+                                const path = action.name === 'orders'
+                                    ? `/${action.name}/${user?._id}`
+                                    : `/${action.name}`;
 
-
-                                }
-                                else if(action.name==='orders'){
-
-                                    return (
-                                        <SpeedDialAction
-                                            key={action.name}
-                                            icon=<Link to={`/${action.name}/${user._id}`}>{action.icon}</Link>
-                                            tooltipTitle={action.name}
-                                        />
-                                    )
-
-
-                                }
-                                else {
-
-                                    return (
-                                        <SpeedDialAction
-                                            key={action.name}
-                                            icon=<Link to={`/${action.name}`}>{action.icon}</Link>
-                                            tooltipTitle={action.name}
-                                        />
-                                    )
-                                }
-                            })}
-                        </SpeedDial>
-                    </div>)
-            }</div>
-
+                                return (
+                                    <SpeedDialAction
+                                        key={action.name}
+                                        icon={<Link to={path}>{action.icon}</Link>}
+                                        tooltipTitle={action.name}
+                                    />
+                                );
+                            }
+                        })}
+                    </SpeedDial>
+                )}
+            </div>
         </header>
-
-
     );
-}
+};
 
 export default Header;
